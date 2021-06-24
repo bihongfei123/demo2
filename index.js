768450b945fc1cb6ddf43abb5f51cf317e133a13
bihongfei <495352715@qq.com>
2021-06-24 17:00:51 +0800

    第一次提交

---
+++ index.js
@@ -0,0 +1,9 @@
+/*
+ * @Description:
+ * @version: 1.0
+ * @Author: hfbi
+ * @Date: 2021-06-24 17:00:24
+ * @LastEditors: Please set LastEditors
+ * @LastEditTime: 2021-06-24 17:00:35
+ */
+console.log("第一次提交");
